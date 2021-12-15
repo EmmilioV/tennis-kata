ce14ecca2f7c0e119eed36b7c78d9ab050b9c345
EmmilioV <68575969+EmmilioV@users.noreply.github.com>
2021-12-15 17:05:12 -0500

    Refactor tennisGame1

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -21,46 +21,64 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-
-        if (m_score1==m_score2)
+        
+        if (isScore1EqualToScore2())
         {
-            return whenEqualScores(score);
+            score= whenEqualScores();
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (isScore1OrScore2GreatterThan4())
         {
-            return whenScoresGreaterThan4(score);
+            score= whenScoresGreaterThan4(score);
         }
-
-        return lessThan4(score);
+        else
+        {
+            score = lessThan4(score);
+        }
+        
+        return score;
     }
 
-    public String whenEqualScores(String score){
+    private boolean isScore1OrScore2GreatterThan4() {
+        return m_score1>=4 || m_score2>=4;
+    }
+
+    private boolean isScore1EqualToScore2() {
+        return m_score1==m_score2;
+    }
+
+    public String whenEqualScores(){
         switch (this.m_score1)
         {
             case 0:
-                score = "Love-All";
-                break;
+                return "Love-All";
             case 1:
-                score = "Fifteen-All";
-                break;
+                return "Fifteen-All";
             case 2:
-                score = "Thirty-All";
-                break;
+                return "Thirty-All";
             default:
-                score = "Deuce";
-                break;
-
+                return "Deuce";
         }
-        return score;
     }
 
     public String whenScoresGreaterThan4(String score){
         int minusResult = m_score1-m_score2;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
+        if (isMinusResultEqualTo1(minusResult)) score ="Advantage player1";
+        else if (isMinusResultEqualToNegative1(minusResult)) score ="Advantage player2";
+        else if (isMinusResultEqualOrGreaterThan2(minusResult)) score = "Win for player1";
         else score ="Win for player2";
         return score;
+    }
+
+    private boolean isMinusResultEqualOrGreaterThan2(int minusResult) {
+        return minusResult>=2;
+    }
+
+    private boolean isMinusResultEqualToNegative1(int minusResult) {
+        return minusResult ==-1;
+    }
+
+    private boolean isMinusResultEqualTo1(int minusResult) {
+        return minusResult==1;
     }
 
     public String lessThan4(String score){
@@ -69,7 +87,7 @@ public class TennisGame1 implements TennisGame {
         {
             if (i==1) tempScore = m_score1;
             else { score+="-"; tempScore = m_score2;}
-            score= concatScore(tempScore, score);
+            score+= concatScore(tempScore, score);
         }
         return score;
     }
@@ -78,18 +96,15 @@ public class TennisGame1 implements TennisGame {
         switch(tempScore)
         {
             case 0:
-                score+="Love";
-                break;
+                return "Love";
             case 1:
-                score+="Fifteen";
-                break;
+                return "Fifteen";
             case 2:
-                score+="Thirty";
-                break;
+                return "Thirty";
             case 3:
-                score+="Forty";
-                break;
+                return "Forty";
+            default: 
+                return score;
         }
-        return  score;
     }
 }
