035e8a831c78654fce13e4992d9efb7e0c0674cb
EmmilioV <68575969+EmmilioV@users.noreply.github.com>
2021-12-16 11:18:57 -0500

    Refactor tennisGame3

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -13,18 +13,55 @@ public class TennisGame3 implements TennisGame {
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        if (isP1AndP2LessThan4() && !isP1PlusP2EqualTo6()) {
+            s = getScoreAccordingToP1();
+            return s;
         }
+        if (isP1EqualToP2())
+            return "Deuce";
+        
+        s = scoreIfP1GreaterThanP2();
+        return thereIsAnAdventageOrWinner(s);
     }
-    
+
+    private String thereIsAnAdventageOrWinner(String s) {
+        return isP1MinusP2SquaredEquals1() ? "Advantage " + s : "Win for " + s;
+    }
+
+    private boolean isP1MinusP2SquaredEquals1() {
+        return (p1-p2)*(p1-p2) == 1;
+    }
+
+    private String scoreIfP1GreaterThanP2() {
+        String s;
+        s = p1 > p2 ? p1N : p2N;
+        return s;
+    }
+
+    private boolean isP1EqualToP2() {
+        return p1 == p2;
+    }
+
+    private String getScoreAccordingToP1() {
+        String s;
+        String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        s = p[p1];
+        s = scoreWhenP1EqualToP2(s, p);
+        return s;
+    }
+
+    private String scoreWhenP1EqualToP2(String s, String[] p) {
+        return (isP1EqualToP2()) ? s + "-All" : s + "-" + p[p2];
+    }
+
+    private boolean isP1AndP2LessThan4() {
+        return p1 < 4 && p2 < 4;
+    }
+
+    private boolean isP1PlusP2EqualTo6() {
+        return p1 + p2 == 6;
+    }
+
     public void wonPoint(String playerName) {
         if (playerName == "player1")
             this.p1 += 1;
